111f00f9ac65d8283f23dfdfe1d0ef6048a26910
Nithin Reddy <tronicskull@gmail.com>
2012-07-05 21:33:56 -0700

    Finished static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home Page" do
+    it "should have the h1 'Nithin Reddy'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Nithin Reddy')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Nithin Reddy | Home")
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Nithin Reddy | Help")
+    end
+  end
+  
+  describe "About page" do
+    it "should have the h1 'About Me'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Me')
+    end
+    it "should have the title 'About Me'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "Nithin Reddy | About Me")
+    end
+  end
+  
+end
